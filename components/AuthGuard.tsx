1a665ed7eb9f71139276ee2a2b5c7506b3a91f10
Aleksander Zebrowski <zebrowski.aleksander@gmail.com>
2025-06-30 23:00:07 +0200

    WORKING VERSION WITH WORKING AUTH!

--- components/AuthGuard.tsx
+++ components/AuthGuard.tsx
@@ -9,7 +9,7 @@ import LoadingSpinner from './LoadingSpinner'
 interface AuthGuardProps {
   children: React.ReactNode
   fallback?: React.ReactNode
-}
+} 
 
 export default function AuthGuard({ children, fallback }: AuthGuardProps) {
   const { user, loading } = useAuth()
